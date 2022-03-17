70c3e9b4832c65f403bf752ec6bb9ef2c49598c4
Siegric <57648341+Siegric@users.noreply.github.com>
2022-03-17 00:31:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
+from datetime import datetime
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import discord
+from discord.ext import tasks
 import asyncio
 from googletrans import Translator
 import string
@@ -13,7 +16,8 @@ tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 translator = Translator()
 chat_history = []
-admins = ['special', 'Siegric#9286', 'lllllll#0997']
+admins = ['special', 'lllllll#0997']
+print(datetime.now().hour)
 
 
 #Saves chat history as a text file in chronological order
@@ -29,6 +33,18 @@ def save_chat_history(user):
         return archive
 
 
+@tasks.loop(minutes=60.0)
+async def daily_tasks():
+    print(datetime.now().hour)
+    if datetime.now().hour == 0:
+        channel = client.get_channel(952924269350912120)
+        key = RandomWords().get_random_word(hasDictionaryDef=True)
+        output = quote_gen(key)
+        embed = discord.Embed(title=output[0], description=f"{output[-1]}, {output[-2]}")
+        embed.set_author(name="Quote of the day:")
+        await channel.send(embed=embed)
+
+
 #Translates arguments and returns translated argument as well as the source language
 def translate(i):
     result = translator.translate(str(i))
@@ -42,25 +58,44 @@ def translate(i):
 
 def index_blank(i):
     for index, character in enumerate(i):
-       if character in string.whitespace:
+        if character in string.whitespace:
             yield index
 
 
-def qotd(*key):
+def quote_gen(key):
+    output = quote(str(key), limit=10)
+    list_of_output = []
+    list_of_author = []
+    list_of_book = []
     try:
-        output = quote(str(key), limit=1)
-        for i in range(len(output)):
-            final_output = (output[i]['quote'])
-            author = (output[i]['author'])
-            print(final_output)
-            return [final_output, author]
-    except:
+        index = len(output)
+        for i in range(index):
+            list_of_output.append(output[i]['quote'])
+            list_of_author.append(output[i]['author'])
+            list_of_book.append(output[i]['book'])
+        rindex = random.randrange(1, index)
+        final_output = list_of_output[rindex]
+        book = list_of_book[rindex]
+        author = list_of_author[rindex]
+        if len(final_output) > 250:
+            final_output = final_output[:250]
+            i = final_output.rsplit('.', 1)
+            final_output = i[0]+"..."
+        return [final_output, book, author]
+    except TypeError:
+        return False
+
+
+def qotd(*key):
+    key = [i for i in key if i]
+    key = ''.join(key)
+    if key:
+        output = quote_gen(str(key))
+        return output
+    else:
         key = RandomWords().get_random_word(hasDictionaryDef=True)
-        output = quote(key, limit=1,)
-        for i in range(len(output)):
-            final_output = (output[i]['quote'])
-            author = (output[i]['author'])
-            return [final_output, author]
+        output = quote_gen(key)
+        return output
 
 
 #Generation method taken and modified from https://huggingface.co/microsoft/DialoGPT-medium#:~:text=DialoGPT%20is%20a%20SOTA%20large,single%2Dturn%20conversation%20Turing%20test.
@@ -102,8 +137,12 @@ class Lilia(discord.Client):
             if i[:4] == "qotd":
                 key = i[5:]
                 output = qotd(key)
-                await message.channel.send(output[0])
-                await message.channel.send(f"-by {output[-1]}")
+                if output is False:
+                    await message.channel.send("Invalid keyword generated, try again!")
+                else:
+                    embed = discord.Embed(title=output[0], description=f"{output[-1]}, {output[-2]}")
+                    embed.set_author(name="Quote of the day:")
+                    await message.channel.send(embed=embed)
             elif i[:9] == "translate":
                 try:
                     index = list(index_blank(i))
@@ -128,4 +167,4 @@ class Lilia(discord.Client):
 
 
 client = Lilia()
-client.run('OTUwMjcwNjI4NzM5NTc5OTA0.YiWefQ.NYPnNLE7YvoOJQSKuSDL_6lCj_w')#Token to run bot
+client.run('OTUwMjcwNjI4NzM5NTc5OTA0.YiWefQ.ezKmTPRTebfMjkvGLaIENrQcLAQ')#Token to run bot
